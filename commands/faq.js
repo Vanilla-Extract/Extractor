34e82f295a23f7d081341b126f9775a78cbbe98c
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-09-10 20:14:57 +0100

    #3 Delete trigger message after using !faq

--- commands/faq.js
+++ commands/faq.js
@@ -7,15 +7,17 @@ module.exports = {
 	execute(message, args) {
         if (args[0]==undefined) message.channel.send(`You need to provide a keyword. Usage: \`${prefix}faq [keyword]\``).then(msg=>msg.delete({timeout:messageTimeout}))
         else {
-            const specifiedKeyword = args[0].toLowerCase()
-            client.channels.cache.get(faqDataChannelID).messages.fetch({limit:1}).then(messages=>{
-                const faqList = JSON.parse(messages.last().content)
-                const keywordsList = faqList.map(i=>i.keyword)
-                if (keywordsList.includes(specifiedKeyword)) {
-                    const faq = faqList[keywordsList.indexOf(specifiedKeyword)]
-                    message.channel.send({embed:{"title":faq.question,"description":faq.answer,"color":color}})
-                }
-                else message.channel.send(`That keyword was invalid. Usage: \`${prefix}faq [keyword]\``).then(message=>message.delete({timeout:messageTimeout}))
+            message.delete().then(()=>{
+                const specifiedKeyword = args[0].toLowerCase()
+                client.channels.cache.get(faqDataChannelID).messages.fetch({limit:1}).then(messages=>{
+                    const faqList = JSON.parse(messages.last().content)
+                    const keywordsList = faqList.map(i=>i.keyword)
+                    if (keywordsList.includes(specifiedKeyword)) {
+                        const faq = faqList[keywordsList.indexOf(specifiedKeyword)]
+                        message.channel.send({embed:{"title":faq.question,"description":faq.answer,"color":color}})
+                    }
+                    else message.channel.send(`That keyword was invalid. Usage: \`${prefix}faq [keyword]\``).then(message=>message.delete({timeout:messageTimeout}))
+                })
             })
         }
 	}
