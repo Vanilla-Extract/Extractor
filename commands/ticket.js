8a34cb49a9124581a20b67ebdde9770d763fcf8e
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-09-23 18:53:29 +0100

    add !ticket command

---
+++ commands/ticket.js
@@ -0,0 +1,19 @@
+const { prefix, color } = require('../config.json')
+
+module.exports = {
+    name: 'ticket',
+    cooldown: 5,
+	execute(message, args) {
+        message.channel.send(`**Rules Of A Ticket**
+        1. Wait *patiently*! There will be times when you won't get an immediate response!
+        2. Don't ask to ask, just ask.
+        3. Don't wait for us! Just say what you want with the format below!
+        
+        What you need help with
+        Whats supposed to happen
+        What happens instead
+        Additional thoughts
+        
+        Example: My dirt texture isn't working! It's supposed to look like this: <image> but ends up using the default one!`)
+    }
+}
