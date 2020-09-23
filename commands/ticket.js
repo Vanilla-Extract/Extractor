526319a06ab7147268cc35466c9261e2ba4667c2
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-09-23 18:54:44 +0100

    fix !ticket

--- commands/ticket.js
+++ commands/ticket.js
@@ -5,15 +5,15 @@ module.exports = {
     cooldown: 5,
 	execute(message, args) {
         message.channel.send(`**Rules Of A Ticket**
-        1. Wait *patiently*! There will be times when you won't get an immediate response!
-        2. Don't ask to ask, just ask.
-        3. Don't wait for us! Just say what you want with the format below!
-        
-        What you need help with
-        Whats supposed to happen
-        What happens instead
-        Additional thoughts
-        
-        Example: My dirt texture isn't working! It's supposed to look like this: <image> but ends up using the default one!`)
+    1. Wait *patiently*! There will be times when you won't get an immediate response!
+2. Don't ask to ask, just ask.
+3. Don't wait for us! Just say what you want with the format below!
+
+What you need help with
+Whats supposed to happen
+What happens instead
+Additional thoughts
+
+Example: My dirt texture isn't working! It's supposed to look like this: <image> but ends up using the default one!`)
     }
 }
