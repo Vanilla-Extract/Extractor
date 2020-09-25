df824008363c82c7d7c08ff8170d3a90a7bf3e1e
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-09-25 20:24:31 +0100

    add !joined command

---
+++ commands/joined.js
@@ -0,0 +1,13 @@
+const { prefix, color } = require('../config.json')
+
+module.exports = {
+    name: 'joined',
+    cooldown: 5,
+	execute(message, args) {
+        if (args[0]==undefined) message.channel.send(`Usage \`${prefix}joined [id]\`\nHow to get a user's id: https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-`)
+        else {
+            const member = message.guild.members.cache.get(args[0])
+            message.channel.send(`\`${member.user.tag}\` joined at \`${member.joinedAt.toUTCString()}\``)
+        }
+    }
+}
