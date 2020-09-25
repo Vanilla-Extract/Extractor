49918da0359f6ff92afce15bcef3c213c4bd9a09
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-09-25 19:33:24 +0100

    update ticket formatting

--- commands/ticket.js
+++ commands/ticket.js
@@ -7,20 +7,17 @@ module.exports = {
         message.delete().then(()=>{
             message.channel.send({embed:{
                 "title": "Rules Of A Ticket",
-                "fields": [
-                    {
-                        "name": ":one:",
-                        "value": "Wait *patiently*! There will be times when you won't get an immediate response!\n"
-                    },
-                    {
-                        "name": ":two:",
-                        "value": "Don't ask to ask, just ask.\n"
-                    },
-                    {
-                        "name": ":three:",
-                        "value": "Don't wait for us! Just say what you want with the format below:\n\nWhat you need help with\nWhats supposed to happen\nWhat happens instead\nAdditional thoughts\n\nExample: My dirt texture isn't working! It's supposed to look like this: <image> but ends up using the default one!"
-                    }
-                ]
+                "description": `**Rules Of A Ticket**
+1. Wait *patiently*! There will be times when you won't get an immediate response!
+2. Don't ask to ask, just ask.
+3. Don't wait for us! Just say what you want with the format below:
+
+What you need help with
+Whats supposed to happen
+What happens instead
+Additional thoughts
+
+Example: My dirt texture isn't working! It's supposed to look like this: <image> but ends up using the default one!`
             }})
         })
     }
