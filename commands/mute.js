229f050debe97924950ec0c3fcfe6311af648124
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-12-05 23:50:27 +0000

    send confirmation message when muting someone

--- commands/mute.js
+++ commands/mute.js
@@ -11,17 +11,20 @@ module.exports = {
 			else if ( args[0].startsWith("<@!") && args[0].endsWith(">") ) id = args[0].substring(4, 21)
 			else if ( args[0].length==18 ) id = args[0]
 			const member = message.guild.members.cache.get(id)
+			const user = client.users.cache.get(id)
 			if (member==undefined) message.channel.send(`Could not find a valid specified user.\nUsage: ${prefix}mute @User [time in hours]`)
 			else {
 				const mutedRole = message.guild.roles.cache.get(mutedRoleID)
 				if (!isNaN(args[1])) {
 					duration = parseFloat(args[1])
 					member.roles.add(mutedRole, `Muted by ${message.author.tag} for ${duration} hours`)
+					message.channel.send(`Muted **${user.tag}** for ${duration} hours.`)
 					setTimeout ( () => {
 						member.roles.remove(mutedRole, `Unmuted automatically after ${duration} hours. Originally muted by ${message.author.tag}`)
 					}, duration*3600000)
 				}
 				member.roles.add(mutedRole, `Muted indefinitely by ${message.author.tag}`)
+				message.channel.send(`Muted **${user.tag}** indefinitely.`)
 			}
 		}
     }
