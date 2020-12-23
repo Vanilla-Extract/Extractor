f0688ff7b1b7baa76ee64ecfd8374743b079c2d4
Chromus_ <Chromus-dev@users.noreply.github.com>
2020-12-23 12:44:59 -0800

    fix color on github

--- commands/github.js
+++ commands/github.js
@@ -6,8 +6,13 @@ module.exports = {
 		message
 			.delete()
 			.then(() =>
-                message.channel.send('Find a link to the GitHub organization here: https://github.com/Vanilla-Extract')
-                message.channel.send({embed:{"title":"Github Organization","description":"Find a link to the GitHub organization here: https://github.com/Vanilla-Extract","color":color}})
+				message.channel.send({
+					embed: {
+						title: 'Github Organization',
+						description: 'Find a link to the GitHub organization here: https://github.com/Vanilla-Extract',
+						color: color
+					}
+				})
 			);
 	}
 };
