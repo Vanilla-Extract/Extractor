87f83807e58534ec7d979189c97f7bb5e9945386
Chromus_ <Chromus-dev@users.noreply.github.com>
2020-12-23 13:17:28 -0800

    fix ping command again

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,6 @@ module.exports = {
 	name: 'ping',
 	cooldown: 0,
 	execute(message, args) {
-		message.channel.send(':ping_pong: Pong!');
-		message.channel.send('The game lasted ${Date.now() - message.createdTimestamp}ms.');
+		message.channel.send(`:ping_pong: Pong! The game lasted ${Date.now() - message.createdTimestamp}ms.`);
 	}
 };
