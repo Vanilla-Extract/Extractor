94cbc29c3718157484e3ab6ef9ec8b18353dd072
Chromus_ <Chromus-dev@users.noreply.github.com>
2020-12-23 12:44:17 -0800

    add color to github start ping command start giveaway command

--- commands/github.js
+++ commands/github.js
@@ -1,7 +1,13 @@
+const { color } = require('../config.json');
 module.exports = {
-    name: "github",
-    cooldown: 5,
+	name: 'github',
+	cooldown: 5,
 	execute(message, args) {
-        message.delete().then(()=>message.channel.send("Find a link to the GitHub organization here: https://github.com/Vanilla-Extract"))
-    }
-}
+		message
+			.delete()
+			.then(() =>
+                message.channel.send('Find a link to the GitHub organization here: https://github.com/Vanilla-Extract')
+                message.channel.send({embed:{"title":"Github Organization","description":"Find a link to the GitHub organization here: https://github.com/Vanilla-Extract","color":color}})
+			);
+	}
+};
