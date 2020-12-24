8b2feb1862a741f41c54e9d2c70bde2fd7fceb15
Chromus_ <Chromus-dev@users.noreply.github.com>
2020-12-24 08:14:02 -0800

    fix cooldown on ping command?

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: 'ping',
-	cooldown: 0,
+	cooldown: 0.001,
 	execute(message, args) {
 		message.channel.send(`:ping_pong: Pong! The game lasted ${Date.now() - message.createdTimestamp}ms.`);
 	}
