82fe8e0ed407d47716d47b1f6752b76396cd3b44
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-12-24 12:32:22 +0000

    add picasso
    fix #9

---
+++ picasso.js
@@ -0,0 +1,46 @@
+const { client } = require("./index")
+const { prefix } = require("./config.json")
+
+const Discord = require("discord.js")
+const scalePixelArt = require("scale-pixel-art")
+const request = require("request").defaults({ encoding: null })
+
+
+client.on("message", message => {
+	
+	if (
+		!message.content.startsWith(`${prefix}scale`) ||
+		message.author.bot
+	) return
+	
+	if (message.reference) scaleAndSend ( message.referencedMessage.attachments.first(), message.channel )
+	else inputAttachment = scaleAndSend (message.attachments.first(), message.channel )
+	
+})
+
+
+const scaleAndSend = (inputAttachment, channel) => {
+
+	if (inputAttachment==undefined) {
+		
+		channel.send("There was no attachment on that message.\nPing me in a message with an image, or ping the bot in a reply to an image to scale it.")
+		return
+	}
+
+	const inputAttachmentURL = inputAttachment.url
+
+	request.get(inputAttachmentURL, (err, res, inputBuffer) => {
+		
+		scalePixelArt(inputBuffer, 20)
+			.then(outputBuffer => {
+				const outputAttachment = new Discord.MessageAttachment(outputBuffer, "response.png")
+				if (Buffer.byteLength(outputBuffer) <= 8000000) {
+					channel.send(outputAttachment)
+						.catch(error => channel.send(`Sending the scaled image failed for the following reason:\n\`${error}\``))
+				} else channel.send("Could not send the scaled image because the file size was too big.")
+			})
+			.catch(error => channel.send(`Scaling the image failed for the following reason:\n\`${error}\``))
+		
+	})
+	
+}
