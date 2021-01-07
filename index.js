4db177e4b5d4b9646c1e6d665b3bcb1ab73e3d96
Chromus-dev <xlchannel6@gmail.com>
2021-01-07 09:40:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const { prefix, verifyChannelID, verifiedRoleID, roleChannelID, roleDataChannelI
 require("dotenv").config()
 const keepAlive = require('./server')
 
+// set up webhook
+const webhookID = process.env.webhookurl.substr(33, 18)
+const webhookToken = process.env.webhookurl.substr(52, 68)
+const creationsWebhook = new Discord.WebhookClient(webhookID, webhookToken)
+
+
 const client = new Discord.Client({partials: ["MESSAGE","CHANNEL","REACTION"]})
 client.commands = new Discord.Collection()
 module.exports.client=client
@@ -83,6 +89,29 @@ client.on("messageReactionRemove", async (reaction, user) => {
 			await reaction.message.guild.members.cache.get(user.id).roles.remove(reactionsConfig[availableReactions.indexOf(reaction.emoji.name)].role)
 		}
 	})
+})
+
+// creations webhook
+// move messages
+client.on("message", message => {
+
+	if (
+		message.channel.id!=process.env.creationschannelid || // the message is not in community creations
+		message.author.id==client.user.id || // the message was sent my the bot
+		message.attachments.array().length>0 || // there is an attachment
+		message.content.includes("http://") || // there is a link
+		message.content.includes("https://") // there is a link
+
+	) return
+	
+	message.delete()
+	message.reply(`your message was deleted because it didn't have an attachment, image or link. Please use <#${process.env.discussionchannelid}> for talking about creations posted in this channel.`).then(response=>response.delete({timeout:15000}))
+
+	if (message.member.nickname==null) name = message.author.username
+	else name = message.member.nickname
+
+	creationsWebhook.send(message.content, { username: name, avatarURL: message.author.avatarURL({dynamic:true}) } )
+
 })
 
 require("./picasso")
