6b3d02a049bcbd365f35210a01b92b47714c21f5
Chromus_ <Chromus-dev@users.noreply.github.com>
2021-02-18 10:06:59 -0800

    thing

--- index.js
+++ index.js
@@ -60,17 +60,18 @@ client.on('message', (message) => {
 	}
 });
 
-client.on('guildMemberAdd', (member) =>
+client.on('guildMemberAdd', (member) => {
 	client.channels.cache
 		.get('751093731171762188')
-		.send(`<@${member.id}> joined the server. \`${member.guild.memberCount}\``)
-);
+		.send(`<@${member.id}> joined the server. \`${member.guild.memberCount}\``);
+	if (member.user.bot)
+		member.roles.add(member.guild.roles.cache.find((role) => role.id == '731568194552070184')); // add bot role
+	else member.roles.add(member.guild.roles.cache.find((role) => role.id == '731570521690472508')); // add member role
+});
 client.on('guildMemberRemove', (member) => {
 	client.channels.cache
 		.get('751093731171762188')
-		.send(`<@${member.id}> left the server. \`${member.guild.memberCount}\``)
-	if (member.user.bot) member.roles.add(member.guild.roles.cache.find(role=>role.id=="731568194552070184")) // add bot role
-	else member.roles.add(member.guild.roles.cache.find(role=>role.id=="731570521690472508")) // add member role
+		.send(`<@${member.id}> left the server. \`${member.guild.memberCount}\``);
 });
 
 client.on('messageReactionAdd', async (reaction, user) => {
